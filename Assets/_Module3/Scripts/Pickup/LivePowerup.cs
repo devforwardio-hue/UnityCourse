01bcf0a0afa8475878e98f1e732a08568fc9b9de
rrockwell1313 <122168611+rrockwell1313@users.noreply.github.com>
2025-10-15 22:11:00 -0500

    Module 3 finish, pseudo scale code

--- Assets/_Module3/Scripts/Pickup/LivePowerup.cs
+++ Assets/_Module3/Scripts/Pickup/LivePowerup.cs
@@ -1,15 +1,34 @@
 using UnityEngine;
 
+
+  public enum PowerupType
+  {
+    Movement,
+    Jump,
+    Scale
+  }
 public class LivePowerup : MonoBehaviour
 {
-  public float powerSpeed = 100f;
-  public float powerTimeMax = 10f;
-  public float currentTime = 0f;
+  public PowerupType type;
+  public float powerValue = 100f;
+  public float maxTime = 10f;
+  public GameObject buffManagerObject;
+  private Buff_Movement buff_movementScript;
+  private Buff_Jump buff_jumpScript;
+  private Buff_Scale buff_scaleScript;
+
 
   // Start is called once before the first execution of Update after the MonoBehaviour is created
+  
   void Start()
   {
-
+    buffManagerObject = GameObject.Find("BuffManager");
+    if(buffManagerObject != null)
+    {
+      buff_movementScript = buffManagerObject.GetComponent<Buff_Movement>();
+      buff_jumpScript = buffManagerObject.GetComponent<Buff_Jump>();
+      buff_scaleScript = buffManagerObject.GetComponent<Buff_Scale>();
+    }
   }
 
   // Update is called once per frame
@@ -20,11 +39,69 @@ public class LivePowerup : MonoBehaviour
 
   private void OnTriggerEnter(Collider col)
   {
-    if (col.gameObject.CompareTag("Player"))
+    if (col.CompareTag("Player"))
     {
-      //control flat value. Time Delta time is controlled in the movement script instead. 
-      col.gameObject.GetComponent<PlayerMovementOld>().moveSpeed += powerSpeed;
-      Destroy(this.gameObject);
+
+
+
+
+      switch (type)
+      {
+        case PowerupType.Movement:
+        {
+            
+            PlayerMovementOld playerMovement = col.GetComponent<PlayerMovementOld>();
+          if (playerMovement != null)
+          {
+            float defaultSpeed = playerMovement.defaultSpeed;
+            playerMovement.moveSpeed = defaultSpeed + powerValue;
+          }
+
+            if (buff_movementScript != null)
+            {
+              buff_movementScript.maxTime = maxTime + Time.time;
+              buff_movementScript.isActive = true;
+            }
+
+            Destroy(this.gameObject);
+          break;
+        }
+
+        case PowerupType.Jump:
+        {
+            JumpingOld jumpingScript = col.GetComponent<JumpingOld>();
+            if (jumpingScript != null)
+            {
+              float defaultForce = jumpingScript.defaultForce;
+              jumpingScript.jumpForce = defaultForce + powerValue;
+            }
+
+            if (buff_jumpScript != null)
+            {
+              buff_jumpScript.maxTime = maxTime + Time.time;
+              buff_jumpScript.isActive = true;
+            }
+            Debug.Log("Jump");
+            Destroy(this.gameObject);
+          break;
+        }
+
+        case PowerupType.Scale:
+        {
+            //in crease the players scale on x, y, and z, to the powerup value.
+            //apply the buff_scaleScript management details
+            Debug.Log("Scale");
+            //Destroy(this.gameObject);
+            break;
+        }
+        default:
+        {
+            Debug.Log("You forgot to set the powerup type");
+          break;
+        }
+      }
+
+      
     }
   }
 
