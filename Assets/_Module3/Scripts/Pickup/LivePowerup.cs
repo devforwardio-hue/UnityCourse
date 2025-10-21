cd6edddd7b8236db7cbe132e3550cc7d4758e61c
typhon9889 <154334219+typhon9889@users.noreply.github.com>
2025-10-20 22:02:15 -0500

    Challenge Komplete

--- Assets/_Module3/Scripts/Pickup/LivePowerup.cs
+++ Assets/_Module3/Scripts/Pickup/LivePowerup.cs
@@ -89,9 +89,20 @@ public class LivePowerup : MonoBehaviour
         case PowerupType.Scale:
         {
             //in crease the players scale on x, y, and z, to the powerup value.
-            //apply the buff_scaleScript management details
-            Debug.Log("Scale");
-            //Destroy(this.gameObject);
+            Transform playerTransform = col.GetComponent<Transform>();
+            if (playerTransform != null)
+            {
+            playerTransform.localScale = new Vector3(powerValue, powerValue, powerValue);
+            }
+            if (buff_scaleScript != null)
+            {
+                buff_scaleScript.maxTime = maxTime + Time.time;
+                buff_scaleScript.isActive = true;
+            }
+                        //apply the buff_scaleScript management details
+                        Debug.Log("Scale");
+            Destroy(this.gameObject);
+            
             break;
         }
         default:
