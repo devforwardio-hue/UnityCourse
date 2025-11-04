cb9c416f8e1cde34679eaeb978d4221a7feacc97
rrockwell1313 <ryanrockwell13@gmail.com>
2025-11-03 18:34:34 -0600

    fix

--- Assets/_Module3/Scripts/Obstacle/LiveSimpleObstacle.cs
+++ Assets/_Module3/Scripts/Obstacle/LiveSimpleObstacle.cs
@@ -1,29 +1,77 @@
 using UnityEngine;
 
+public enum ObstacleType
+{
+  X, Y, Z
+}
 public class LiveSimpleObstacle : MonoBehaviour
 {
-    public float speed = 2f;      // Speed of Object
-    public float distance = 3f;   // Range of Object
-    public float rotationSpeed = 90f;  // Degrees per second for  any axis rotation
+  public ObstacleType type;
+  public float moveDistance = 5f;
+  public float moveSpeed = 1f;
+  public Transform obstacleTransform;
+  public Vector3 startPosition;
+  private int obstacleDirection = 1;
 
-    private Vector3 startPosition;
+  void Awake()
+    {
+      obstacleTransform = GetComponent<Transform>();
+    }
 
     void Start()
     {
-        // Store the starting position of the object
-        startPosition = transform.position;
+      // Record the initial position so we can compare movement along the selected axis
+      startPosition = obstacleTransform.position;
     }
 
-    void Update()
+  // Update is called once per frame
+  void Update()
     {
-        // Move the object along X and Z using sine and cosine 
-        float x = Mathf.Sin(Time.time * speed) * distance;
-        float z = Mathf.Cos(Time.time * speed) * distance;
+      ObstacleMovement();
+     }
 
-        // Apply the new position
-        transform.position = startPosition + new Vector3(x, 0, z);
-
-        //transform.Rotate(0f, rotationSpeed * Time.deltaTime, 0f, Space.Self);  //X-Axis Movement spin
-        transform.Rotate(0f, 0f, rotationSpeed * Time.deltaTime, Space.Self);    //Z-Axis diagonal spin
+  public void ObstacleMovement()
+  {
+    float posMoving = moveSpeed * obstacleDirection * Time.deltaTime;
+    float currentAxisPosition = 0f;
+    float offsetFromStart = 0f;
+    float xpos = 0f;
+    float ypos = 0f;
+    float zpos = 0f;
+    Debug.Log(currentAxisPosition);
+    // Set movement along one axis explicitly
+    switch (type)
+    {
+      case ObstacleType.X:
+        xpos = posMoving;
+        currentAxisPosition = obstacleTransform.position.x;
+        offsetFromStart = obstacleTransform.position.x - startPosition.x;
+        Debug.Log(offsetFromStart);
+        break;
+      case ObstacleType.Y:
+        ypos = posMoving;
+        currentAxisPosition = obstacleTransform.position.y;
+        offsetFromStart = obstacleTransform.position.y - startPosition.y;
+        break;
+      case ObstacleType.Z:
+        zpos = posMoving;
+        currentAxisPosition = obstacleTransform.position.z;
+        offsetFromStart = obstacleTransform.position.z - startPosition.z;
+        break;
+      default:
+        xpos = posMoving;
+        break;
     }
-}
+
+
+    obstacleTransform.position += new Vector3(xpos, ypos, zpos);
+    if (offsetFromStart >= moveDistance)
+    {
+      obstacleDirection = -1;
+    }
+    else if (offsetFromStart <= -moveDistance)
+    {
+      obstacleDirection = 1;
+    }
+  }
+ }
