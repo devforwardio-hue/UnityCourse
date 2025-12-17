f536c43d9af447aaeaa09fd7fc2951def77e0e37
Chris <magicman3ooo@yahoo.com>
2025-12-17 14:21:33 -0600

    Update Dump*

---
+++ Assets/Chris' Stuff/Random/Key Quest Stuff/AWinnerIsYou.cs
@@ -0,0 +1,106 @@
+﻿using UnityEngine;
+
+
+public class AWinnerIsYou : MonoBehaviour
+{
+    public enum AxisMovement
+    {
+        X, Y, Z
+    }
+    public enum AxisRotation
+    {
+        X, Y, Z
+    }
+
+    public AxisMovement AxisMove;
+    public AxisRotation AxisRotate;
+    public float moveDistance = 5f;
+    public float moveSpeed = 1f;
+    public float rotationSpeed = 200f;
+    public Transform obstacleTransform;
+    public Vector3 startPosition;
+    private int obstacleDirection = 1;
+
+    void Awake()
+    {
+        obstacleTransform = GetComponent<Transform>();
+    }
+
+    void Start()
+    {
+        // Record the initial position so we can compare movement along the selected axis
+        startPosition = obstacleTransform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ObstacleMovement();
+
+        ObstacleRotation();
+    }
+
+    public void ObstacleMovement()
+    {
+        float posMoving = moveSpeed * obstacleDirection * Time.deltaTime;
+        float currentAxisPosition = 0f;
+        float offsetFromStart = 0f;
+        float xpos = 0f;
+        float ypos = 0f;
+        float zpos = 0f;
+        Debug.Log(currentAxisPosition);
+        // Set movement along one axis explicitly
+        switch (AxisMove)
+        {
+            case AxisMovement.X:
+                xpos = posMoving;
+                currentAxisPosition = obstacleTransform.position.x;
+                offsetFromStart = obstacleTransform.position.x - startPosition.x;
+                Debug.Log(offsetFromStart);
+                break;
+            case AxisMovement.Y:
+                ypos = posMoving;
+                currentAxisPosition = obstacleTransform.position.y;
+                offsetFromStart = obstacleTransform.position.y - startPosition.y;
+                break;
+            case AxisMovement.Z:
+                zpos = posMoving;
+                currentAxisPosition = obstacleTransform.position.z;
+                offsetFromStart = obstacleTransform.position.z - startPosition.z;
+                break;
+            default:
+                xpos = posMoving;
+                break;
+        }
+
+
+        obstacleTransform.position += new Vector3(xpos, ypos, zpos);
+        if (offsetFromStart >= moveDistance)
+        {
+            obstacleDirection = -1;
+        }
+        else if (offsetFromStart <= -moveDistance)
+        {
+            obstacleDirection = 1;
+        }
+    }
+
+    public void ObstacleRotation()
+    {
+        switch (AxisRotate)
+        {
+            case AxisRotation.X:
+                obstacleTransform.Rotate(Vector3.right * rotationSpeed * Time.deltaTime);
+                break;
+            case AxisRotation.Y:
+                obstacleTransform.Rotate(Vector3.up * rotationSpeed * Time.deltaTime);
+                break;
+            case AxisRotation.Z:
+                obstacleTransform.Rotate(Vector3.forward * rotationSpeed * Time.deltaTime);
+                break;
+            default:
+                obstacleTransform.Rotate(Vector3.up * rotationSpeed * Time.deltaTime);
+                break;
+        }
+    }
+}
